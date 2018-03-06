40839a3ae67c9d7ab7a6d66027cf36d613f9b705
boopathycse <37016449+boopathycse@users.noreply.github.com>
2018-03-06 17:39:45 +0530

    Create reverse.c

---
+++ reverse.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main()
+{
+   char a[100];
+ 
+   printf("Enter a string to reverse\n");
+   gets(a);
+ 
+   strrev(a);
+ 
+   printf("\nReverse of the string is %s", arr);
+ 
+   return 0;
+}

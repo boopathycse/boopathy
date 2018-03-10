a271abdf5a51a8dec38597c3ea6090d2354bcbe4
boopathycse <37016449+boopathycse@users.noreply.github.com>
2018-03-10 22:18:53 +0530

    Create noofword.c

---
+++ noofword.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+ int main()
+{
+    char s[200];
+    int c = 0, i;
+ 
+    printf("enter string\n");
+    scanf("%[^\n]s", s);
+    for (i = 0;s[i] != '\0';i++)
+    {
+        if (s[i] == ' ')
+            c++;    
+    }
+    printf("number of words in given string are: %d\n", c + 1);
+ return 0;
+}

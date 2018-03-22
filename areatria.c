077c143fdc151084cf0a87567434810ed20728cc
boopathycse <37016449+boopathycse@users.noreply.github.com>
2018-03-22 17:49:36 +0530

    Create areatria.c

---
+++ areatria.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main()
+{
+	int h,b,area;
+	printf("enter the height and base of a triangle");
+	scanf("%d%d",&h,&b);
+	area=h*b/2;
+	printf("the area of a triangle is%d",area);
+	return 0;
+}
+ 

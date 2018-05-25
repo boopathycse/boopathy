675d812fade19ba6a86b4b0460c9271d825185f8
boopathycse <37016449+boopathycse@users.noreply.github.com>
2018-05-25 10:06:54 +0530

    Create numornot.c

---
+++ numornot.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+int main()
+{
+	char s[100];
+	scanf("%c",&s[100]);
+	if(s[100]>='a' && s[100]<='z')
+	{
+		printf("no");
+	}
+	else
+	{
+		printf("yes");
+	}
+	return 0;
+}
